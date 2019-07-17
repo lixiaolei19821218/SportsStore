3d226aa6d902dc4fa50d648c719871be88927a43
lixiaolei19821218 <735534185@qq.com>
2019-07-17 09:30:25 +0800

    Add project files.

---
+++ Await/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Await
+{
+    class Program
+    {
+        //创建计时器
+        private static readonly Stopwatch Watch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            //启动计时器
+            Watch.Start();
+
+            const string url1 = "http://www.cnblogs.com/";
+            const string url2 = "http://www.cnblogs.com/liqingwen/";
+
+            //两次调用 CountCharacters 方法（下载某网站内容，并统计字符的个数）
+            Task<int> t1 = CountCharactersAsync(1, url1);
+            Task<int> t2 = CountCharactersAsync(2, url2);
+
+            //Console.WriteLine($"{url1} 的字符个数：{t1.Result}");
+            //Console.WriteLine($"{url2} 的字符个数：{t2.Result}");
+
+            //三次调用 ExtraOperation 方法（主要是通过拼接字符串达到耗时操作）
+            for (var i = 0; i < 3; i++)
+            {
+                ExtraOperation(i + 1);
+            }
+
+            //控制台输出
+            Console.WriteLine($"{url1} 的字符个数：{t1.Result}");
+            Console.WriteLine($"{url2} 的字符个数：{t2.Result}");
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 统计字符个数
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        private static async Task<int> CountCharactersAsync(int id, string address)
+        {
+            var wc = new WebClient();
+            Console.WriteLine($"开始调用 id = {id}：{Watch.ElapsedMilliseconds} ms");            
+            var result = await wc.DownloadStringTaskAsync(address);
+            Console.WriteLine($"调用完成 id = {id}：{Watch.ElapsedMilliseconds} ms");
+
+            return result.Length;
+        }
+
+        public static int BigTask()
+        {
+            Thread.Sleep(10000);            
+            return 0;
+        }
+
+       
+
+        /// <summary>
+        /// 额外操作
+        /// </summary>
+        /// <param name="id"></param>
+        private static void ExtraOperation(int id)
+        {
+            //这里是通过拼接字符串进行一些相对耗时的操作
+            var s = "";
+
+            for (var i = 0; i < 6000; i++)
+            {
+                s += i;
+            }
+
+            Console.WriteLine($"id = {id} 的 ExtraOperation 方法完成：{Watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
