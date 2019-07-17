6d5d835ba16c4ddff5997f5bdb02f57951519dbd
lixiaolei19821218 <735534185@qq.com>
2019-07-17 09:35:51 +0800

    test

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -23,7 +23,7 @@ namespace SportsStore
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
-            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddScoped(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc();
             services.AddMemoryCache();
